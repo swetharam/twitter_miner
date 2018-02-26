9198d9cf4654926f67c784f2df13d3b5d395c39c
Swetha Ramaswamy <swetharamaswamy66@gmail.com>
2018-02-25 18:57:11 -0600

    tokens hidden in this one

--- maincode.py
+++ maincode.py
@@ -1,10 +1,10 @@
 import tweepy
 from textblob import TextBlob
-consumer_key= 'Quo2d5FNN6F9LNwqOExTufMxR'
-consumer_secret='qKHT8wy8PGrf7NcsONuUtk0RWhnvCrYbNp6NPqFOJbR4ZQldsT'
+consumer_key= '*********************'
+consumer_secret='************************'
 
-access_token='914577085347323906-YtVrpvFXrUZHt5v6rTdnl0tr6KY0cjs'
-access_token_secret='rZzCmPxP5ZS8FNfXt4MT3QAzuAfFPCOeSmDx44XaaEahs'
+access_token='************************'
+access_token_secret='*******************'
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
